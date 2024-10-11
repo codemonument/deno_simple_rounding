039343baa0f75689363b764fb862ffa8c665c199
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-11 13:15:52 +0200

    add examples to all functions

--- mod.ts
+++ mod.ts
@@ -39,14 +39,14 @@
  *
  * @example
  * ```ts
- *   import { assertEquals } from "@std/assert";
- *   import { roundToPrecision } from "@codemonument/simple-rounding";
+ * import { assertEquals } from "@std/assert";
+ * import { roundToPrecision } from "@codemonument/simple-rounding";
  *
- *   const result1 = roundToPrecision(55.3218697, 2);
- *   assertEquals(result1, 55.32);
+ * const result1 = roundToPrecision(55.3218697, 2);
+ * assertEquals(result1, 55.32);
  *
- *   const result2 = roundToPrecision(55.3258697, 2);
- *   assertEquals(result2, 55.33);
+ * const result2 = roundToPrecision(55.3258697, 2);
+ * assertEquals(result2, 55.33);
  * ```
  */
 export function roundToPrecision(value: number, precision: number): number {
@@ -61,6 +61,17 @@ export function roundToPrecision(value: number, precision: number): number {
  * @param value The floating point value to be ceiled
  * @param precision The number of decimal places to ceil to
  * @returns The ceiled number
+ *
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ * import { ceilToPrecision } from "@codemonument/simple-rounding";
+ *
+ * const result1 = ceilToPrecision(55.3218697, 2);
+ * assertEquals(result1, 55.33);
+ * const result2 = ceilToPrecision(55.3258697, 2);
+ * assertEquals(result2, 55.33);
+ * ```
  */
 export function ceilToPrecision(value: number, precision: number): number {
     const p = Math.pow(10, precision || 0);
@@ -74,6 +85,17 @@ export function ceilToPrecision(value: number, precision: number): number {
  * @param value The floating point value to be floored
  * @param precision The number of decimal places to floor to
  * @returns The floored number
+ *
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ * import { floorToPrecision } from "@codemonument/simple-rounding";
+ *
+ * const result1 = floorToPrecision(55.3218697, 2);
+ * assertEquals(result1, 55.32);
+ * const result2 = floorToPrecision(55.3258697, 2);
+ * assertEquals(result2, 55.32);
+ * ```
  */
 export function floorToPrecision(value: number, precision: number): number {
     const p = Math.pow(10, precision || 0);
@@ -85,6 +107,17 @@ export function floorToPrecision(value: number, precision: number): number {
  * @param value The floating point value to be truncated
  * @param precision The number of decimal places to truncate to
  * @returns The truncated number
+ *
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ * import { truncToPrecision } from "@codemonument/simple-rounding";
+ * const result1 = truncToPrecision(55.3218697, 3);
+ * assertEquals(result1, 55.321);
+ * const result2 = truncToPrecision(55.3258697, 3);
+ * assertEquals(result2, 55.325);
+ *
+ * ```
  */
 export function truncToPrecision(value: number, precision: number): number {
     return (value < 0 ? ceilToPrecision : floorToPrecision)(value, precision);
@@ -95,6 +128,16 @@ export function truncToPrecision(value: number, precision: number): number {
  * @param value The floating point value to be formatted
  * @param precision The number of decimal places to format to
  * @returns The formatted number as string
+ *
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ * import { toFixed } from "@codemonument/simple-rounding";
+ * const result1 = toFixed(55.3218697, 2);
+ * assertEquals(result1, "55.32");
+ * const result2 = toFixed(55.3258697, 2);
+ * assertEquals(result2, "55.33");
+ * ```
  */
 export function toFixed(value: number, precision: number): string {
     return roundToPrecision(value, precision).toFixed(precision);
